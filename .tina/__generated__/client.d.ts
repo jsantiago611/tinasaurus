7f806d2df8e57ad71a4c7ca4631e60875772de22
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-11-04 22:56:20 +0000

    Update Tina CMS schema

---
+++ .tina/__generated__/client.d.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/workspaces/tinasaurus/.tina/__generated__/.cache/1730760948512', url: 'https://content.tinajs.io/1.5/content/2174382d-831e-45da-bfc7-5a73f8c426ba/github/main', token: 'a60f67ad68273ba2faa8a0795de032ae948a30f2', queries,  });
+export default client;
+  
